d50215cc71db177bf9770792f1cc93a041003f30
Justin <thehunterofshadows@gmail.com>
2019-04-20 11:30:11 -0500

    Final draft before workingn on readme and codebook.

--- run_analysis.R
+++ run_analysis.R
@@ -9,14 +9,14 @@ readMergeData<-function(){
   #instead of doing this, merge each new fread it should work since it's in order.
   
   #combine all of the test columns
-  testData<<-fread("data/test/subject_test.txt",sep= " ", header=FALSE, col.names = "subjectID") %>%
-    cbind(fread("data/test/y_test.txt",sep= " ", header=FALSE, col.names = "activityID")) %>%
-    cbind(fread("data/test/X_test.txt",sep= " ", header=FALSE, col.names=read.csv("data/features.txt", sep=" ", header=FALSE, colClasses = "character")[,2]))
+  testData<<-fread("UCI HAR Dataset/test/subject_test.txt",sep= " ", header=FALSE, col.names = "subjectID") %>%
+    cbind(fread("UCI HAR Dataset/test/y_test.txt",sep= " ", header=FALSE, col.names = "activityID")) %>%
+    cbind(fread("UCI HAR Dataset/test/X_test.txt",sep= " ", header=FALSE, col.names=read.csv("UCI HAR Dataset/features.txt", sep=" ", header=FALSE, stringsAsFactors = FALSE)[,2]))
   
   #combine all of the subject columns to the test columns, thus combing the test and subject data.
-  fullData<<-fread("data/train/subject_train.txt",sep= " ", header=FALSE, col.names = "subjectID") %>%
-    cbind(fread("data/train/y_train.txt",sep= " ", header=FALSE, col.names = "activityID")) %>%
-    cbind(fread("data/train/X_train.txt",sep= " ", header=FALSE, col.names=read.csv("data/features.txt", sep=" ", header=FALSE, colClasses = "character")[,2])) %>%
+  fullData<<-fread("UCI HAR Dataset/train/subject_train.txt",sep= " ", header=FALSE, col.names = "subjectID") %>%
+    cbind(fread("UCI HAR Dataset/train/y_train.txt",sep= " ", header=FALSE, col.names = "activityID")) %>%
+    cbind(fread("UCI HAR Dataset/train/X_train.txt",sep= " ", header=FALSE, col.names=read.csv("UCI HAR Dataset/features.txt", sep=" ", header=FALSE, stringsAsFactors = FALSE)[,2])) %>%
     rbind(testData)
   
   
@@ -33,14 +33,23 @@ meanStd<-function(){
 addDescActivityNames<-function(){
   #read in the activities labels
   #then createa a new column matching up the activity lables with the y data.
-  myLabels<-fread("data/activity_labels.txt", sep=" ",header=FALSE, col.names=c("activityID","activityLabel"))
+  myLabels<-fread("UCI HAR Dataset/activity_labels.txt", sep=" ",header=FALSE, col.names=c("activityID","activityLabel"))
   meanStd<<-merge(myLabels, meanStd, by="activityID", all = TRUE)
+  meanStd$activityID<<-NULL
 }
-returnmeanStd<-function(){
-  return(meanStd)
+
+avgActivities<-function(){
+  dataAvg<<-group_by(meanStd,activityLabel,subjectID) %>%
+    summarise_each(list(mean))
+}
+
+returnTidyData<-function(){
+
+  return(dataAvg)
 }
 
 readMergeData()
 meanStd()
 addDescActivityNames()
+avgActivities()
 
